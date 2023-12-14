36f29a15959897f24fae28db48a93c25c336fd9e
Aquasunil <aquasunil429@gmail.com>
2023-12-14 20:41:00 +0500

    task: anagram(done)

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,7 @@
 */
 
 function isAnagram(str1, str2) {
-
+  return str1.trim().toLowerCase().split('').sort().join('') === str2.trim().toLowerCase().split('').sort().join('');
 }
 
 module.exports = isAnagram;
